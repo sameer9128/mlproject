d2649b232806ab33f45947d58a15d1f31c3a2fa5
sameer ahmad <sameer.engi9128@gmail.com>
2025-03-07 17:44:20 +0530

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -17,7 +17,7 @@ def error_message_detail(error,error_detail:set):
      
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
